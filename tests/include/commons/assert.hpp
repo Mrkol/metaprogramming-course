728411bd0add683a6cd3affa7c93e9ffe6715494
Raed Romanov <raid_r@mail.ru>
2020-09-20 23:48:13 +0300

    Add test build system and tests for task 1

---
+++ tests/include/commons/assert.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <cstdlib>
+#include <iostream>
+#include <concepts>
+#include <optional>
+
+
+#define ensure(type, ...) detail::assert_##type(__FILE__, __LINE__, __VA_ARGS__)
+
+namespace detail {
+
+void fail(const char* file, size_t line) {
+    std::cerr << "Assertion failed: file \"" << file << "\", line " << line << std::endl;
+    std::abort();
+}
+
+void assert_true(const char* file, size_t line, const std::convertible_to<bool> auto& v1) {
+    if (!v1) {
+        fail(file, line);
+    }
+}
+
+void assert_eq(const char* file, size_t line, const auto& v1, const auto& v2) {
+    assert_true(file, line, v1 == v2);
+}
+
+template <class T>
+void assert_nullopt(const char* file, size_t line, const std::optional<T>& v) {
+    assert_true(file, line, !v.has_value());
+}
+
+}
