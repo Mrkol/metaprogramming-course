1a9bd01e55b56f9c87d4798178bf063a354d2273
Raed Romanov <raid_r@mail.ru>
2020-10-04 14:29:00 +0300

    tests: add nocompile test for task 1

--- tests/task1/nocompile.cpp
+++ tests/task1/nocompile.cpp
@@ -1,14 +1,4 @@
 #include <string_mapper.hpp>
-#include <commons/concepts.hpp>
-
-#include <concepts>
-#include <optional>
-#include <iostream>
-
-
-template <class M, class Base, class To>
-concept Mapper = mpg::Complete<M> && std::is_same_v<std::optional<To>, decltype(M::map(std::declval<const Base&>()))>;
-
 
 class Animal {
 public:
@@ -18,16 +8,13 @@ public:
 class Cat : public Animal {};
 class Dog : public Animal {};
 
-class Val {};
-struct ExtVal : public Val {};
+struct Val {
+    /* implicit */ operator int() const noexcept {
+        return 0;
+    }
+};
 
-using MyMapper1 = ClassMapper<
-    Animal, Val,
-    Mapping<Cat, Val{}>,
-    Mapping<Dog, ExtVal{}>
->;
-
-using MyMapper2 = ClassMapper<
+using MyMapper = ClassMapper<
     Animal, int,
     Mapping<Cat, 2>,
     Mapping<Dog, Val{}>
@@ -36,6 +23,5 @@ using MyMapper2 = ClassMapper<
 
 int main() {
     Cat cat;
-    MyMapper1::map(cat);
-//    MyMapper2::map(cat);
+    MyMapper::map(cat);
 }
